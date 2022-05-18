0065fef39c5e5a33b87909b7097047b5339f4aa3
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-05-18 10:07:41 +0000

    05

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -1,4 +1,8 @@
-hrs = int(input("Enter Hours:"))
-rate = float(input("Enter Rate:"))
-pay = hrs*rate
-print("Pay:",pay)
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter the Rate:")
+r = float(rate)
+if h <= 40:
+ 	print( h  * r)
+elif h > 40:
+	print(40 * r + (h-40)*1.5*r)
