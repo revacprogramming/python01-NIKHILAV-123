95ccc7a96c38073acfa36633a317cf48fa999a54
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-06-27 04:44:22 +0000

    files1.py

--- ActivitySet01/files1.py
+++ ActivitySet01/files1.py
@@ -3,3 +3,17 @@ count = 0
 for line in fhand:
     count = count + 1
 print('Line Count:', count)
+
+
+'''~/python01-NIKHILAV-123$ cd ActivitySet01
+~/python01-NIKHILAV-123/ActivitySet01$ ls
+files1.py     problem05.py  problem12.py  problem19.py
+files3.py     problem06.py  problem13.py  problem20.py
+files.py      problem07.py  problem14.py  problem21.py
+files.txt     problem08.py  problem15.py  problem22.py
+problem02.py  problem09.py  problem16.py  problem23.py
+problem03.py  problem10.py  problem17.py
+problem04.py  problem11.py  problem18.py
+~/python01-NIKHILAV-123/ActivitySet01$ python files1.py
+Line Count: 1909
+~/python01-NIKHILAV-123/ActivitySet01$ '''
