b45a8c5c82c76b9061da611bde71847dd9359487
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-06-27 04:50:15 +0000

    files.py

--- ActivitySet01/files.py
+++ ActivitySet01/files.py
@@ -10,4 +10,9 @@ for line in fh:
         total=total+num
 average=total/count
 print("Average spam confidence:",average)
-        
+
+
+
+'''/python01-NIKHILAV-123/ActivitySet01$ python files.py
+Enter file name: files.txt
+Average spam confidence: 0.7507185185185187'''
