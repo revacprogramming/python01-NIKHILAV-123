21bda8b5d358515d1b28e780e6b81a79a5830ade
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-07-11 04:14:00 +0000

    10.

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,7 +1,7 @@
 # Dictionaries
 
 #filename = "dataset/mbox-short.txt"
-name = input("dataset/mbox-short.txt")
+name = input("Enter file name:")
 handle = open(name)
 mails = dict()
 for line in handle:
@@ -15,3 +15,24 @@ for key,value in  mails.items():
         maxi_value = value
         maxi_key = key
 print(maxi_key, maxi_value)
+
+
+
+
+
+
+'''~/python01-NIKHILAV-123/ActivitySet01$ cd ActivitySet01
+bash: cd: ActivitySet01: No such file or directory
+~/python01-NIKHILAV-123/ActivitySet01$ ls
+files1.py     problem05.py  problem12.py  problem19.py
+files3.py     problem06.py  problem13.py  problem20.py
+files.py      problem07.py  problem14.py  problem21.py
+files.txt     problem08.py  problem15.py  problem22.py
+problem02.py  problem09.py  problem16.py  problem23.py
+problem03.py  problem10.py  problem17.py
+problem04.py  problem11.py  problem18.py
+~/python01-NIKHILAV-123/ActivitySet01$ python problem10.py
+Enter file name:files.txt
+cwen@iupui.edu 5
+~/python01-NIKHILAV-123/ActivitySet01$ '''
+
