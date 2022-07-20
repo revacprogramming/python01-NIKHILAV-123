41c75af3a135adef2e571fff72f42fc1d9a629a2
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-07-20 09:38:16 +0000

    12

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,2 +1,12 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+string="Python is fun"
+s=re.search('Python',string)
+print(s)
+
+
+import re
+txt="The rain in banglore"
+x=re.findall("[a-z]",txt)
+print(x)
