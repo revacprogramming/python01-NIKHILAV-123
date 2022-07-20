46384274f3b68ae8d4255e0daedbd0593b7a5900
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-07-20 09:31:10 +0000

    15

--- ActivitySet01/problem15.py
+++ ActivitySet01/problem15.py
@@ -1,2 +1,22 @@
 # Object Oriented Programming
 # https://www.py4e.com/lessons/Objects
+class student:
+  def __init__(self,name,srn):
+    self.name=name
+    self.srn=srn
+  def display(self):
+    print("My name is {0} and srn is {1}".format(self.name,self.srn))
+s1=student("nikhil","r21ef238")
+s1.display()
+
+#ENCAPSULATION
+class Computer:
+  '''program to demonstrate Encapsulation'''
+  def __init__(self):
+    self.__maxprice=900
+  def sell(self):
+    print("selling price is:",self.__maxprice)
+  def setmaxprice(self,price):
+    self.__maxprice=price
+c1=Computer()
+c1.sell( )
